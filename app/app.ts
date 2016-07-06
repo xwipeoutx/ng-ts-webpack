dafce571261af644a0e7a25c016562036b7741c9
XwipeoutX <xwipeoutx@gmail.com>
2016-07-06 23:41:42 +1000

    ES2015-ified the angular imports

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
+import * as angular from "angular"
 import HomeModule from "./home/home"
-import "angular";
 import "./app.scss";
 
 var AppModule = angular.module("app", [
--- app/home/home.ts
+++ app/home/home.ts
@@ -1,4 +1,4 @@
-import "angular";
+import * as angular from "angular"
 import "./home.scss"
 
 var HomeModule = angular.module("app.home", []);
